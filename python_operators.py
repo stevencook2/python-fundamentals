50072ab50866d2e0becfcf888207c673c826310c
EXT-Cook, Steven B <steven.b.cook4@boeing.com>
2022-12-20 17:26:22 -0600

    Updated Operators exercise

--- python_operators.py
+++ python_operators.py
@@ -7,23 +7,32 @@ b = 4
 c = "7"
 
 # Add a and b together, print the output
-
+add1 = a + b
+print(add1)
 
 # Add a and c together - what do you need to do to make this work? print the output
-
+add2 = a + int(c)
+print(add2)
 
 # Subtract a from b, print the output
-
+sub = b - a
+print(sub)
 
 # create a new variable z that is the modulus of a and b - print the result
-
+z = a % b
+print(z)
 
 # Print the result of raising a to the power of b
-
+exp = a**b
+print(exp)
 
 # What is the difference between the standard divide arithmetic operator and the floor division operator (//)?
 # Print both and determine the difference
+div = a / b
+div2 = a // b
 
+
+print(div,div2)
 # ----------------------------------------------------------------------------------------------------------------------
 # Assignment operators
 
@@ -39,17 +48,37 @@ print(f'the new value for a is {a}')
 print(f'the equivalent long form would have been a = a + 4')
 
 # -=
+print('subtracting 4 from a')
+a -= 4
+print(f'the new value for a is {a}')
+print(f'the equivalent long form would have been a = a - 4')
+
 
 # *=
-
+print('multiplying 4 and a')
+a *= 4
+print(f'the new value for a is {a}')
+print(f'the equivalent long form would have been a = a * 4')
 # /=
-
+print('dividing a and 4')
+a /= 4
+print(f'the new value for a is {a}')
+print(f'the equivalent long form would have been a = a / 4')
 # %=
-
+print('getting modulus of a and 4')
+a %= 4
+print(f'the new value for a is {a}')
+print(f'the equivalent long form would have been a = a % 4')
 # //=
-
+print('dividing a and 4 and getting only integer')
+a //= 4
+print(f'the new value for a is {a}')
+print(f'the equivalent long form would have been a = a / 4')
 # **=
-
+print('taking a to the power of 4')
+a **= 4
+print(f'the new value for a is {a}')
+print(f'the equivalent long form would have been a = a ** 4')
 # other operators include &=, |=, ^=, >>= and <<=
 # try them out later
 
@@ -61,9 +90,14 @@ print(f'the equivalent long form would have been a = a + 4')
 if a == b:
     print(f'variable a which is set to {a} is equivalent to b which is set to {b}')
 
-# try printing the statement a == b what result would you expect to receive?
+# try printing the statement a == b what result would you expect to receive? False Boolean
 # can you add equivalent if statements to evaluate if a is greater than or less than b, printing the result?
-
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('a is less than b')
+else:
+    print('a and b are equivalent')
 # ----------------------------------------------------------------------------------------------------------------------
 # Logical operators
 
@@ -81,6 +115,9 @@ else:
 
 # can you write a similar statement to take action if either of the statements are true?
 
+if (h == i) or (j == k):
+    print('One of these are true')
+
 # ----------------------------------------------------------------------------------------------------------------------
 # Membership
 
@@ -96,6 +133,11 @@ else:
 
 # write a similar if / else statement to determine if a number does NOT exist within the list, printing the
 # appropriate messages
+
+if z not in my_list:
+    print(f'my_list does not contain the number {z}')
+else:
+    print(f'my_list contains the number {z}')
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Bitwise operators
